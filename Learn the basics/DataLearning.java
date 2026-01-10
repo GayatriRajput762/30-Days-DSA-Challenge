38398a5e4a0350d680dcad5b2bb84854052502ce
GayatriRajput762 <gayatrirajput762@gmail.com>
2026-01-10 20:05:31 +0530

    verygood

---
+++ Learn the basics/DataLearning.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+
+public class DataLearning {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        System.out.println(age);
+        String name = sc.next();
+        System.out.println(name);
+
+    }
+    
+}
