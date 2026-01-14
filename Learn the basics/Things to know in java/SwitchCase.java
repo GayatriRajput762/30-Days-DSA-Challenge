79919a3239795c45df1b1ebe28d5c780870fa652
GayatriRajput762 <gayatrirajput762@gmail.com>
2026-01-14 19:23:35 +0530

    best

---
+++ Learn the basics/Things to know in java/SwitchCase.java
@@ -0,0 +1,17 @@
+public class SwitchCase {
+    public static void main(String[] args) {
+        String days = "name";
+        switch(days){
+            case "monday" : System.out.println("first day of the week");
+            break;
+            case"tuesday":System.out.println("seconday of the week");
+            break;
+            case "wednesday":System.out.println("third day of the week");
+            break;
+            default :System.out.println("invalid day");
+        }
+        }
+        
+    }
+    
+
