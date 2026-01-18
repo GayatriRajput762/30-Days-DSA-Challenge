f1d245282588f4af572af793134d08d88c38ede8
GayatriRajput762 <gayatrirajput762@gmail.com>
2026-01-18 21:52:28 +0530

    good

---
+++ Learn the basics/Things to know in java/ForLoopExample.java
@@ -0,0 +1,13 @@
+public class ForLoopExample {
+    public static void main(String[] args) {
+        int num =10;
+        for(int i =1;i<=num;i++){
+            if(i%2==0){
+                System.out.println("number is even");
+            }else{
+
+            }System.out.println("number is odd");
+        }
+    }
+    
+}
