e88de103b5fd599baeb62ae5c771375254b98160
GayatriRajput762 <gayatrirajput762@gmail.com>
2026-01-25 21:59:12 +0530

    excellent

---
+++ Learn the basics/Things to know in java/SumExample.java
@@ -0,0 +1,11 @@
+public class SumExample {
+    public static void main(String[] args) {
+        // variable
+        int a =20;
+        int b = 10;
+        int sum = a+b;
+        System.out.println(sum);
+
+    }
+    
+}
