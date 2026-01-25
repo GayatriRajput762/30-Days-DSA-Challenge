4d0101564600b6b3f4e81f74533df23afb5d9c85
GayatriRajput762 <gayatrirajput762@gmail.com>
2026-01-25 22:19:39 +0530

    good

---
+++ Learn the basics/Things to know in java/Difference.java
@@ -0,0 +1,9 @@
+public class Difference {
+    public static void main(String[] args) {
+        int a =20;
+        int b = 30;
+        int diff =b-a;
+        System.out.println(diff);
+    }
+    
+}
