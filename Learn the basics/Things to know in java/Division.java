afa369baf0c6bbde3ce5d96c14d41fa19da5f164
GayatriRajput762 <gayatrirajput762@gmail.com>
2026-01-26 12:48:14 +0530

    best

---
+++ Learn the basics/Things to know in java/Division.java
@@ -0,0 +1,9 @@
+public class Division {
+    public static void main(String[] args) {
+        int a =20;
+        int b =10;
+        int div =a/b;
+        System.out.println(div);
+    }
+    
+}
