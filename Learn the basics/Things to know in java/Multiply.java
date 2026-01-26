5619146549227783bd17685a3dc25d502d85e1cf
GayatriRajput762 <gayatrirajput762@gmail.com>
2026-01-26 12:40:39 +0530

    better

---
+++ Learn the basics/Things to know in java/Multiply.java
@@ -0,0 +1,11 @@
+public class Multiply {
+    public static void main(String[] args) {
+        // variable
+        int a =30;
+        int b =10;
+        int mul =a*b;
+        System.out.println(mul);
+
+    }
+    
+}
